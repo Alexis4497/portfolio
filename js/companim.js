59b4d0419665a01a3ea41c16c2fb7fbe10790adb
Alexis Caillé <alexiscaille97@gmail.com>
2021-02-24 16:21:05 +0100

    responsive design

--- js/companim.js
+++ js/companim.js
@@ -1,7 +1,21 @@
-jQuery(document).ready(function(){
-	jQuery('.skillbar').each(function(){
-		jQuery(this).find('.skillbar-bar').animate({
-			width:jQuery(this).attr('data-percent')
-		},6000);
-	});
-});
+window.addEventListener('scroll', function(){
+
+	console.log(window.innerHeight);
+	console.log(window.scrollY);
+	console.log(document.body.offsetHeight);
+
+	scrollValue = (window.innerHeight + window.scrollY) / (document.body.offsetHeight);
+
+	console.log(scrollValue);
+
+	if(scrollValue > 0.55){
+
+		jQuery(document).ready(function(){
+			jQuery('.skillbar').each(function(){
+				jQuery(this).find('.skillbar-bar').animate({
+					width:jQuery(this).attr('data-percent')
+				},6000);
+			});
+		});
+	}
+})
