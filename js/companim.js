ea4e2ee2f304d0bb86e961ab3db3f250f7793b69
Alexis Caillé <alexiscaille97@gmail.com>
2021-04-14 18:08:00 +0200

    design

--- js/companim.js
+++ js/companim.js
@@ -1,12 +1,12 @@
 window.addEventListener('scroll', function(){
 
-	console.log(window.innerHeight);
-	console.log(window.scrollY);
-	console.log(document.body.offsetHeight);
+	//console.log(window.innerHeight);
+	//console.log(window.scrollY);
+	//console.log(document.body.offsetHeight);
 
 	scrollValue = (window.innerHeight + window.scrollY) / (document.body.offsetHeight);
 
-	console.log(scrollValue);
+	//console.log(scrollValue);
 
 	if(scrollValue > 0.55){
 
